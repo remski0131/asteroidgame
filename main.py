7ae16f2f60232606351311faeac2eeebc5cbe3b5
Jeffrey Wang <jeffreywang@Jeffreys-MacBook-Air.local>
2024-11-11 00:42:05 -0800

    player shape

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     time = pygame.time.Clock()
     dt = 0
@@ -15,8 +17,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = time.tick(60) / 1000
+        
+    
 
 
 
